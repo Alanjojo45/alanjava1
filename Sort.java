b737c162b79747999aac7a62d8d953dfe9dfed64
Alanjojo45 <alanjojo45@gmail.com>
2026-08-02 23:43:56 +0530

    Implement Bubble Sort for user-input array
    This program takes user input for an array of integers and sorts them using the Bubble Sort algorithm, then prints the sorted array.

---
+++ Sort.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SortArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements (n): ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Sorting using Bubble Sort algorithm
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.print("Array sorted in ascending order: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        sc.close();
+    }
+}
+
