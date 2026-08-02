359ccffe160702a7215f5f20e9fe4053ad96dd75
Alanjojo45 <alanjojo45@gmail.com>
2026-08-02 23:42:41 +0530

    Add ArrayReverseDisplay class to reverse an array

---
+++ Reverse.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ArrayReverseDisplay {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements (n): ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " numbers:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Display in same order
+        System.out.print("Original order: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        // Display in reverse order
+        System.out.print("Reverse order: ");
+        for (int i = n - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        sc.close();
+    }
+}
+
